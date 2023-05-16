1c9452b3e7c595b14b562337b5885709601ff81a
Shailesh Aanand <anaandshailu@gmail.com>
2023-05-16 17:50:12 +0530

    add checks for env vars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import { z } from "zod";
+
 import app, { log } from "./app.js";
+try {
+  z.object({
+    SESSION_SECRET: z.string().nonempty(),
+    REDIS_URL: z.string().nonempty(),
+    DATABASE_URL: z.string().nonempty(),
+  }).parse(process.env);
+} catch (e) {
+  if (e instanceof z.ZodError) {
+    log.error({ errors: e.errors });
+    process.exit(1);
+  } else {
+    throw e;
+  }
+}
 
 const port = process.env.PORT || 3000;
 await app.listen(port);
