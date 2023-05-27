29dd38f3a568371e1b591babfb535beb1e8fd227
Shailesh Aanand <anaandshailu@gmail.com>
2023-05-27 11:47:34 +0530

    print errors as info

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  req.log.error(err);
+  req.log.info(err);
   if (err instanceof z.ZodError) {
     err.errors.map((error) =>
       req.flash("error", `${error.path.join(",")} : ${error.message}`)
